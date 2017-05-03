e79e9e00f9a1d3a6a864fc5681ad4d016bbd9916
Andrew Landsverk <dewdew@gmail.com>
2017-05-02 21:16:45 -0500

    remove unused method

--- src/main/java/com/quantasnet/defender/build/BuildDependencyRepository.java
+++ src/main/java/com/quantasnet/defender/build/BuildDependencyRepository.java
@@ -1,10 +1,6 @@
 package com.quantasnet.defender.build;
 
-import com.quantasnet.defender.dependency.Dependency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface BuildDependencyRepository extends JpaRepository<BuildDependency, Long> {
-    List<BuildDependency> findAllByDependency(Dependency dependency);
 }
