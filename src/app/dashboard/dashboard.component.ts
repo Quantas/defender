afcd1fdc59bb11cd61c69b213172c75308e84db6
Andrew Landsverk <dewdew@gmail.com>
2017-05-23 22:50:43 -0500

    show recent builds on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 
 import 'rxjs/add/operator/map';
+import { Page } from '../table/page';
+import { Column } from '../table/column';
+import { JavaDatePipe } from '../core/javadate.pipe';
+import { PageChangeEvent } from '../table/page.change.event';
 
 @Component({
   templateUrl: 'dashboard.component.html',
@@ -12,6 +16,15 @@ export class DashboardComponent implements OnInit {
   depCount = 0;
   buildCount = 0;
   appCount = 0;
+
+  recentBuilds: Page;
+
+  buildsTableColumns: Column[] = [
+    { header: 'Group ID', property: 'app.groupId', alignRight: true },
+    { header: 'Artifact ID', property: 'app.artifactId', alignRight: true },
+    { header: 'Version', property: 'version', alignRight: true },
+    { header: 'Build Time', property: 'buildTime', alignRight: true, pipe: new JavaDatePipe() },
+  ];
 
   constructor(private http: Http) {
   }
@@ -27,6 +40,14 @@ export class DashboardComponent implements OnInit {
 
     this.http.get('/api/apps/count').map((res) => res.json()).subscribe((count) => {
       this.appCount = count;
+    });
+
+    this.refreshRecentBuilds(undefined);
+  }
+
+  refreshRecentBuilds(pageChangeEvent: PageChangeEvent): void {
+    this.http.get('/api/builds/recent').map((res) => res.json()).subscribe((page: Page) => {
+      this.recentBuilds = page;
     });
   }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { TableModule } from '../table/table.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
+    TableModule,
     RouterModule.forChild([
       { path: '', component: DashboardComponent, pathMatch: 'full'}
     ])
