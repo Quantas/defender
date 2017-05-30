f02b4a8bea6a066a71d9e47c201b6d87ea74f2bf
Andrew Landsverk <dewdew@gmail.com>
2017-05-29 19:05:45 -0500

    clean up status component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 import { JavaDatePipe } from './javadate.pipe';
 import { TitleCasePipe } from './titlecase.pipe';
 import { StatusComponent } from './status.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  imports: [
+    CommonModule
+  ],
   exports: [
     JavaDatePipe,
     TitleCasePipe,
