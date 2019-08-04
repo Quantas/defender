f9211338f68d7c289f2b9ce0fbb1c12e3469a20f
Andrew Landsverk <dewdew@gmail.com>
2019-08-04 16:44:06 -0500

    Angular material update

--- defender-client/src/app/app.module.ts
+++ defender-client/src/app/app.module.ts
@@ -8,7 +8,11 @@ import { AppComponent } from './app.component';
 import { HttpResponseInterceptor } from './response.intereptor';
 import { InfoService } from './info/info.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
